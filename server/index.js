788075355b8ae69dc8ac7da491ebecf6290beece
DHEERAJ KUMAR PAMPARI <dheerajpampari234@gmail.com>
2025-02-28 15:46:48 -0800

    change-5

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import mongoose from "mongoose"
-
+import authRoutes from "./routes/AuthRoutes.js"
 dotenv.config()
 
 const app=express();
 const port = process.env.PORT || 3001;
-const databaseURL = process.env.DATABASE_URL;
+const databaseURL = "mongodb+srv://admin:admin@cluster0.76a8z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 app.use(cors({
     origin:[process.env.ORIGIN],
     methods:["GET","POST","PUT","PATCH","DELETE"],
@@ -17,11 +17,11 @@ app.use(cors({
 );
 app.use(cookieParser());
 app.use(express.json());
+app.use('./api/auth',authRoutes)
 const server = app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`);
 })
 console.log("databaseURL",databaseURL);
-mongoose
-.connect(databaseURL,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=> console.log('DB Connection Successfully'))
+mongoose.connect(databaseURL)
+.then((con)=> console.log('DB Connection Successfully',con))
 .catch((err)=>console.log(err.message))
