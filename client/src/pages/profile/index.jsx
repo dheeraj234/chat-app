64238be8d6b6338a12ec04dc22a6122f22a00b98
DHEERAJ KUMAR PAMPARI <dheerajpampari234@gmail.com>
2025-03-05 16:58:13 -0800

    change-11

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,23 +1,109 @@
 import { useAppStore } from "@/store"
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import {IoArrowBack}from "react-icons/io5"
-const Profile=()=>{
-    const {userInfo}=useAppStore()
-    const [firstName,setFirstName]=useState("")
-    const [lastName,setLastName]=useState("")
-    const [image,setImage]=useState(null)
-    const [hovered,setHovered]=useState(false)
-    const [selectedcolor,setSelectedColor]=useState(0)
-    const saveChanges=async()=>{}
+import { IoArrowBack } from "react-icons/io5"
+import { Faplus, FaTrash } from "react-icons/fa"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { colors, getColor } from "@/lib/utils"
+import { Toaster } from "@/components/ui/sonner"
+import {toast}from "sonner"
+import { apiClient } from "@/lib/api-client"
+import { UPDATE_PROFILE_ROUTE } from "@/utils/constants"
+const Profile = () => {
+    const navigate = useNavigate()
+    const { userInfo, setUserInfo } = useAppStore()
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [image, setImage] = useState(null)
+    const [hovered, setHovered] = useState(false)
+    const [selectedColor, setSelectedColor] = useState(0);
+    const validateProfile=()=>{
+        if(!firstName){
+            toast.error("First Name is required.")
+            return false
+        }
+        if(!lastName){
+            toast.error("Last Name is required.");
+            return false
+        }
+        return true
+    }
+    const saveChanges = async () => {
+        if(validateProfile()){
+            try {
+                const response= await apiClient.post(
+                    UPDATE_PROFILE_ROUTE,{firstName,lastName,color:selectedColor},
+                    {withCredentials:true}
+                );
+                if(response.status===200 && response.data){
+                    setUserInfo({...response.data});
+                    toast.success("Profile updated successfully.");
+                    navigate("/chat");
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+     }
     return (
-    <div className="bg-[#1b1c24] h-[100vh] flex justify-center flex-col gap-10">
-        <div className="flex flex-col gap-10 md:w-max">
-            <div>
-                <IoArrowBack className="text-4xl lg:text-6xl"/>
+        <div className="bg-[#1b1c24] h-[100vh] flex justify-center flex-col gap-10">
+            <div className="flex flex-col gap-10 md:w-max">
+                <div>
+                    <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+                </div>
+                <div className="grid grid-cols-2">
+                    <div className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center"
+                        onMouseEnter={() => setHovered(true)}
+                        onMouseLeave={() => setHovered(false)}
+                    >
+                        <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+                            {image ? <AvatarImage src={image} alt="profile" className="object-cover w-full h-full bg-black"
+                            />
+                                : <div className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex justify-center items-center rounded-full ${getColor(selectedColor)}`}>
+                                    {firstName ? firstName.split("").shift()
+                                        : userInfo.email.split("").shift()}
+                                </div>}
+                        </Avatar>
+                        {
+                            hovered && (
+                                <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full">
+                                    {image ? <FaTrash className="text-white text-3xl cursor-pointer" />
+                                        : <Faplus className="text-white text-3xl cursor-pointer" />}
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className="flex min-w-32 flex-col gap-5 text-white items-center justify-center">
+                        <div className="w-full">
+                            <Input placeholder="email" type="email" disabled value={userInfo.email} className="rounded-lg p-6 bg-[#2c2e3b] border-none"/>
+                        </div>
+                        <div className="w-full">
+                            <Input placeholder="First Name" type="text" onChange={(e)=>setFirstName(e.target.value)} value={firstName} className="rounded-lg p-6 bg-[#2c2e3b] border-none"/>
+                        </div>
+                        <div className="w-full">
+                            <Input placeholder="Last Name" type="text" nChange={(e)=>setLastName(e.target.value)} value={lastName} className="rounded-lg p-6 bg-[#2c2e3b] border-none"/>
+                        </div>
+                        <div className="w-full flex gap-5">
+                            {colors.map((color,index)=> 
+                            <div 
+                            className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300
+                            ${ selectedColor === index ? "outline outline-white/50 outline-4" :""}
+                            `}
+                            key={index}
+                            onClick={()=>setSelectedColor(index)}
+                            >
+
+                            </div> )}
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full">
+                    <Button className="h-16 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300"
+                    onClick={saveChanges}
+                    >Save Changes</Button>
+                </div>
             </div>
-        </div>
-       
-    </div>)
+
+        </div>)
 }
 export default Profile
--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { login, signup, getUserInfo} from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
 const authRoutes=Router();
 authRoutes.post("/signup",signup)
 authRoutes.post("/login",login)
 authRoutes.get("/user-info",verifyToken,getUserInfo)
+authRoutes.post("/update-profile",verifyToken,updateProfile)
 export default authRoutes
